4db8fd58078b195cbcd01935a9457b42beba094e
dovalle90 <48726598+dovalle90@users.noreply.github.com>
2020-05-18 20:25:04 -0600

    Curse controller added

---
+++ unittestdemo/src/main/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/unittestdemo/controller/CurseController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.controller;
+
+/**
+ *
+ * @author diegoovalle
+ */
+public class CurseController {
+    
+}
