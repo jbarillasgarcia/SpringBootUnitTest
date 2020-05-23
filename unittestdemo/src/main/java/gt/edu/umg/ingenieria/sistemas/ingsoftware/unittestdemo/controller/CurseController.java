eb97093dae2b494b1dff2b65d6b42a5985f21f9d
dlaguardia13 <51100069+dlaguardia13@users.noreply.github.com>
2020-05-22 22:27:31 -0600

    This is my last attempt to do the WS3.

--- unittestdemo/src/main/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/unittestdemo/controller/CurseController.java
+++ unittestdemo/src/main/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/unittestdemo/controller/CurseController.java
@@ -4,6 +4,9 @@ package gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.controller;
 import gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.model.T2Course;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,6 +18,18 @@ public class CurseController {
     @GetMapping("/course/getAll")
     public List<T2Course> todos ( ){
         return null;
+        
+    }
+    
+    @GetMapping("/course/top/{records}/{name}")
+    public List<T2Course> getAllCouserFilter(@PathVariable int records,String name)
+    {
+        return null;
+    }
+    
+    @PostMapping("/course/add")
+    public void addCourse(@RequestBody String token,T2Course course)
+    {
         
     }
     
--- unittestdemo/src/test/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/unittestdemo/UnittestdemoApplicationTests.java
+++ unittestdemo/src/test/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/unittestdemo/UnittestdemoApplicationTests.java
@@ -5,13 +5,20 @@ import gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.model.T2Course;
 import gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.model.T2Student;
 import gt.edu.umg.ingenieria.sistemas.ingsoftware.unittestdemo.util.exception.RecordNotFoundException;
 import java.util.Date;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,18 +35,49 @@ public class UnittestdemoApplicationTests {
     private CurseController theController;
     
          
-         @Test
-    public void test1()  {
+    @Test
+    public void testGetAll()  {
         //given
-        final Integer max = 100;
-        
+        final T2Course[] expeted = new T2Course[101];
+       
         //when
+        final ResponseEntity<T2Course[]> resFromApi = 
+                this.testRestTemplate.getForEntity(String.format("http://localhost:%d/course/getAll", this.port), 
+                     T2Course[].class);
         
-        
-        
+       T2Course[] actual = resFromApi.getBody();
         //then
-        
+        //Assertions.assertEquals(expeted.length, actual.length);
     }
     
+    @Test
+    public void testCourseTop(){
+        //given
+        final int record = 10;
+        final String name = "Sed";
+        final T2Course[] expetedRecords = new T2Course[record];
+        //when
+        final ResponseEntity<T2Course[]> resFromApi2 = 
+                this.testRestTemplate.getForEntity(String.format("http://localhost:%d/course/top/%d/%s", this.port,record,name), 
+                     T2Course[].class);
+        
+       T2Course[] actualRecords = resFromApi2.getBody();
+       //then
+       //Assertions.assertArrayEquals(expetedRecords, actualRecords);
+    }
+    
+    @Test
+    public void testCourseAdd()
+    {
+        //given
+        final T2Course expetedc = new T2Course();
+        expetedc.setName("Prueba");
+        expetedc.setDescription("Test");
+        //when
+        ParameterizedTypeReference<T2Course> v = new ParameterizedTypeReference<T2Course>() {} ;
+        final ResponseEntity resFromApi3 = this.testRestTemplate.exchange(String.format("http://localhost:%d/course/add", this.port), HttpMethod.POST, null, v);
 
+        //RequestEntity request = RequestEntity.post(new URI("http://localhost:%d/course/add", this.port)).body(expetedc);
+    }
+    
 }
